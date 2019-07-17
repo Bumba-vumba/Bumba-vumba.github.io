8bedce1df70f1f8817ba75fa6b0c2e59ba61ea77
Bumba-vumba <51786799+Bumba-vumba@users.noreply.github.com>
2019-07-17 10:43:01 +0300

    Add files via upload

---
+++ project.js
@@ -0,0 +1,21 @@
+var p;
+var butt;
+function Delete()
+{ 
+  p.parentNode.removeChild(p);
+  butt.parentNode.removeChild(butt);
+
+}
+function CreateElement()
+{   p = document.createElement('p');
+    butt = document.createElement('button');
+    p.innerHTML = document.getElementById('kk').value;
+	document.body.appendChild(p);
+	p.setAttribute("id", 'id1');
+	p.setAttribute('align', 'center');
+    butt.innerHTML = 'x';
+	butt.setAttribute('onclick', 'Delete()');
+	butt.setAttribute('align', 'center');
+	document.body.appendChild(butt);
+	
+}
