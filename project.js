0029e3ff4f2274b4df02119992417ef42c70c106
buversson <vlad-zhizhin@list.ru>
2019-07-23 19:38:13 +0300

    fix delete items (fix #5)

--- project.js
+++ project.js
@@ -1,28 +1,31 @@
 var p;
 var butt;
-var list=[];
+var item=[];
+var item2=[];
+var list = new Array(item, item2);
 
+function Draw()
+{	
+	var div = document.getElementById('items');
+	div.innerHTML = '';   
+	p = document.createElement('p');
+    butt = document.createElement('button');
+    butt.innerHTML = 'x';
+    butt.setAttribute('onclick', 'Delete()');                          
+	p.innerHTML = document.getElementById('kk').value;
+	item2.push(p.innerHTML); 
+ 	item.push(butt.innerHTML);
+ 	document.body.appendChild(butt);
+ 	document.body.appendChild(p);                               
+	
+}
 function Delete()
 { 
-  p.parentNode.removeChild(p);
-  butt.parentNode.removeChild(butt);
-
-}
-function CreateElement()
-{   p = document.createElement('p');
-    butt = document.createElement('button');
-    p.innerHTML = document.getElementById('kk').value;
-	document.body.appendChild(p);
-	p.setAttribute("id", 'id1');
-	p.setAttribute('align', 'center');
-    butt.innerHTML = 'x';
-	butt.setAttribute('onclick', 'Delete()');
-	butt.setAttribute('align', 'center');
-	document.body.appendChild(butt);
-	list.push(p,butt);
+  
 }
 
-document.getElementById('kk').onkeypress = function()
+document.getElementById('kk').onkeypress = function ()
 {
 	if (event.keyCode === 13) CreateElement()
+   
 }
